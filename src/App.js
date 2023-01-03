e392186988c9b6ad94767d5fe4e3438f744f9676
dong53358 <dong53358@naver.com>
2023-01-04 00:21:38 +0900

    feat: kanbanBoard 추가,삭제 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,15 +1,60 @@
+import { useState } from "react";
 import { useRecoilState } from "recoil";
+import styled from "styled-components";
+import BoardItem from "./components/BoardItem";
 import { kanbanBoardData } from "./recoil/atoms";
 
+const Main = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+`;
+
+const Issue = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 1000px;
+  height: 400px;
+  border: 1px solid black;
+  padding: 10px;
+`;
+
+const Title = styled.span`
+  padding: 10px;
+  font-size: 50px;
+  font-weight: bold;
+`;
+
 function App() {
-  const [data, setData] = useRecoilState(kanbanBoardData);
+  const [datas, setData] = useRecoilState(kanbanBoardData);
+  const [title, setTitle] = useState("");
+
+  const onTitleChange = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const dateTime = Date.now();
+    setData((prev) => [...prev, { title, dateTime }]);
+    setTitle("");
+  };
+
   return (
-    <>
-      <div>hello, world</div>
-      {data.map((data, index) => (
-        <span key={index}>{data}</span>
-      ))}
-    </>
+    <Main>
+      <Issue>
+        <Title>Issue</Title>
+        <form onSubmit={onSubmit}>
+          <input value={title} onChange={onTitleChange} />
+          <button type="submit">추가</button>
+        </form>
+        {datas.map((data, index) => (
+          <BoardItem key={index} data={data} />
+        ))}
+      </Issue>
+    </Main>
   );
 }
 
--- src/recoil/atoms.js
+++ src/recoil/atoms.js
@@ -1,6 +1,23 @@
 import { atom } from "recoil";
 
+const localStorageEffect =
+  (key) =>
+  ({ setSelf, onSet }) => {
+    const savedValue = localStorage.getItem(key);
+    // setSelf -> Callbacks to set or reset the value of the atom.
+    if (savedValue != null) {
+      setSelf(JSON.parse(savedValue));
+    }
+    // setSelf -> Callbacks to set or reset the value of the atom.
+    onSet((newValue, _, isReset) => {
+      isReset
+        ? localStorage.removeItem(key)
+        : localStorage.setItem(key, JSON.stringify(newValue));
+    });
+  };
+
 export const kanbanBoardData = atom({
   key: "data",
-  default: ["1", "2", "3"],
+  default: [],
+  effects: [localStorageEffect("data")],
 });
